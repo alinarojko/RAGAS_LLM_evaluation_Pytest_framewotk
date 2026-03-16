e0d40811e9dbb8f6bb5e8f76fe04616cbd7c2fd1
rojko.alina <rojko.alina@gmail.com>
2026-03-16 12:14:26 +0000

    test_factual_correctness , multiple metrics evaluation

---
+++ tests/test_factual_correctness.py
@@ -0,0 +1,34 @@
+import pytest
+from langchain_community.utilities import requests
+from ragas import SingleTurnSample, EvaluationDataset, evaluate
+from ragas.metrics import ResponseRelevancy
+from ragas.metrics.collections import FactualCorrectness
+
+from utils.utils import get_llm_response, load_test_data
+
+
+@pytest.mark.parametrize("get_data", load_test_data("test_factual_correctness.py"), indirect=True)
+@pytest.mark.asyncio
+async def test_relevance_factual(llm_wrapper, get_data):
+    metrics = [ResponseRelevancy(llm=llm_wrapper),
+               FactualCorrectness(llm=llm_wrapper)]
+
+    eval_dataset = EvaluationDataset([get_data])
+    results = evaluate(dataset=eval_dataset,metrics=metrics)
+    print(results)
+    for result in results:
+        assert result["answer_relevancy"] > 0.8
+
+
+@pytest.fixture
+def get_data(request):
+    test_data = requests.param
+    responseDict = get_llm_response(test_data)
+
+    sample = SingleTurnSample(
+        user_input=test_data["question"],
+        response=responseDict["answer"],
+        retrieved_contexts=[doc["page_content"] for doc in responseDict.get("retrieved_docs")],
+        reference=test_data["reference"]
+    )
+    return sample
